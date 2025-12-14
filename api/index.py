4c018fb5577a34562d9385cea743407062405223
BuildTools <rising1legend@gmail.com>
2025-12-14 05:38:40 +0530

    final?

--- api/index.py
+++ api/index.py
@@ -1,14 +1,25 @@
 from flask import Flask
-
+from flask import request, jsonify
+import base64 as b64
 app = Flask(__name__)
+new_uri = "None"
 
-@app.route('/')
+@app.route('/post')
 def home():
-    return 'Hello, World!'
+    args = request.args
+    global new_uri
+    new_uri = args['uri']
+    new_uri = b64.b64decode(new_uri).decode('utf-8')
+    print(new_uri)
+    return 'OK', 200
 
-@app.route('/about')
+@app.route('/ping')
+def ping():
+    return 'pong', 200
+@app.route('/get')
 def about():
-    return 'About'
+    global new_uri
+    return new_uri
 
-
-# new thing you think of?
+if __name__ == "__main__":
+    app.run(debug=True)
